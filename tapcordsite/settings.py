f55ced602c2ce6bf33fca07c9080b41f58236447
myfrank4everreal <myfrank4everreal@gmail.com>
2023-10-29 19:49:16 +0100

    added whitenoise to settings middleware

--- tapcordsite/settings.py
+++ tapcordsite/settings.py
@@ -57,6 +57,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    # added the whitenoise to be able to serve static files on deployment
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -144,8 +147,7 @@ STATICFILES_DIRS = [
     # os.path.join(BASE_DIR, 'travel/static'),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
