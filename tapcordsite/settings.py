cfd77d01bcc6edde1264cdffd26524ce0357eaff
myfrank4everreal <myfrank4everreal@gmail.com>
2023-10-29 19:10:15 +0100

    added static root and staticfiles_storage

--- tapcordsite/settings.py
+++ tapcordsite/settings.py
@@ -135,12 +135,16 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'tapcordsite/static/'),
     # os.path.join(BASE_DIR, 'travel/static'),
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
