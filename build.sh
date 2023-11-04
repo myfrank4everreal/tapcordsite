d93d938c1aa7122247916616d4b556f1df154618
myfrank4everreal <myfrank4everreal@gmail.com>
2023-11-04 05:14:52 +0100

    added admin manager code snipet to render environment variable

--- build.sh
+++ build.sh
@@ -4,8 +4,15 @@ python manage.py collectstatic --no-input
 python manage.py migrate 
 
 
-
-
+# creating admin user on render
+# added this to my environment variable list on render dashboard: 
+# Key = Value
+# -----------------
+# CREATE_SUPERUSER = True
+# DJANGO_SUPERUSER_EMAIL = <...>
+# DJANGO_SUPERUSER_PASSWORD = <...>
+# DJANGO_SUPERUSER_USERNAME =<...>
+# this is the code snipet to run django admin on render on free plan
 if [[ $CREATE_SUPERUSER ]];
 then
   python world_champ_2022/manage.py createsuperuser --no-input
